e60c541c1d99c114f73a927bf7f5103f62204a31
Joelle  Johnson <joellej221@gmail.com>
2023-11-07 16:20:05 -0500

    Minor styling changes

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -3,17 +3,17 @@ import { Button, Box, ButtonIcon, ButtonText } from "@gluestack-ui/themed"
 export default function Controls({ setVolume, volume }){
     return(
         <Box flexDirection="row" flexWrap="wrap" justifyContent="center" mt="$8">
-            <Button m="$2" rounded="$xl" size="xl" onPress={() => setVolume(volume + 250)}>
+            <Button m="$4" rounded="$xl" size="xl" onPress={() => setVolume(volume + 250)}>
                 <ButtonText >
                     Add 🥛
                 </ButtonText>
             </Button>
-            <Button m="$2" rounded="$xl" size="xl" onPress={() => setVolume(volume + 750)}>
+            <Button m="$4" rounded="$xl" size="xl" onPress={() => setVolume(volume + 750)}>
                 <ButtonText>
                     Add 🫙
                 </ButtonText>
             </Button>
-            <Button m="$2" rounded="$xl" size="xl" onPress={() => setVolume(0)}>
+            <Button m="$4" rounded="$xl" size="xl" action="negative" onPress={() => setVolume(0)}>
                 <ButtonText>
                     Reset
                 </ButtonText>
