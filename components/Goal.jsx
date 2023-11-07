16f817375622bc4858a8884ab90f3b0ecc10d96f
Joelle  Johnson <joellej221@gmail.com>
2023-11-07 15:43:29 -0500

    Goal tracker color is better

--- components/Goal.jsx
+++ components/Goal.jsx
@@ -7,8 +7,8 @@ export default function Goal({ volume }){
     return(
         <VStack justifyContent="space-around" h={180} alignItems="center" py="$8" px="$4">
             <Text color="$blue800" fontSize="lg" fontWeight="500">Goal: {GOAL} mL</Text>
-            <Progress w={300} size="lg" value={100 * (volume / GOAL)}>
-                <ProgressFilledTrack />
+            <Progress w={300} size="lg" value={100 * (volume / GOAL)} bg="$blue200">
+                <ProgressFilledTrack bg="$blue500"/>
             </Progress>
             <Text color="$blue800" fontWeight="500" fontSize="xl">Today's total: {volume} mL</Text>
         </VStack>
